dc224cba539266868a224848a6601555bed96d9d
Yu-Chun Fang <yuchuf@uw.edu>
2023-04-11 13:46:59 -0700

    Update integration test

--- tests/integration/test_integration.py
+++ tests/integration/test_integration.py
@@ -13,28 +13,24 @@ def test_health_route():
 
 
 def test_rps_route():
-    response = requests.post(f"{APP_URL}/rps", json={"move": "Rock"})
-    assert response.status_code == 200
-    # mapping = ["Rock", "Paper", "Scissors"]
-    # for move in mapping:
-    #     response = requests.post(
-    #         f"{APP_URL}/rps",
-    #         data=json.dumps(dict(move=move)),
-    #         content_type="application/json",
-    #     )
-    #     assert response.status_code == 200
-    #     data = json.loads(response.data)
+    # response = requests.post(f"{APP_URL}/rps", json={"move": "Rock"})
+    # assert response.status_code == 200
+    mapping = ["Rock", "Paper", "Scissors"]
+    for move in mapping:
+        response = requests.post(f"{APP_URL}/rps", json={"move": move})
+        assert response.status_code == 200
+        data = json.loads(response.data)
 
-    #     result, game_result, pc_choice = (
-    #         data["result"],
-    #         data["game_result"],
-    #         int(data["pc_choice"]),
-    #     )
-    #     if game_result == 0:
-    #         assert result == "Tie"
+        result, game_result, pc_choice = (
+            data["result"],
+            data["game_result"],
+            int(data["pc_choice"]),
+        )
+        if game_result == 0:
+            assert result == "Tie"
 
-    #     elif game_result == 1:
-    #         assert result == f"You win, {move} beats {mapping[pc_choice]}"
+        elif game_result == 1:
+            assert result == f"You win, {move} beats {mapping[pc_choice]}"
 
-    #     elif game_result == -1:
-    #         assert result == f"I win, {mapping[pc_choice]} beats {move}"
+        elif game_result == -1:
+            assert result == f"I win, {mapping[pc_choice]} beats {move}"
